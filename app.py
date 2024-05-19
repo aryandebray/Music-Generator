487214fa2f2e0f19bccbac9e38d99e6bd589dbca
Aryan Debray <aryandebray@gmail.com>
2024-05-20 03:14:56 +0530

    Added files

---
+++ app.py
@@ -0,0 +1,76 @@
+from audiocraft.models import MusicGen
+import streamlit as st
+import os
+import torch
+import torchaudio
+import numpy as np
+import base64
+
+
+def load_model():
+    model=MusicGen.get_pretrained("facebook/musicgen-small")
+    return model
+
+def generate_music_tensors(desc,dur:int):
+    print("Description: ",desc)
+    print("Duration: ",dur)
+    model=load_model()
+    model.set_generation_params(
+        use_sampling=True,
+        top_k= 250,
+        duration=dur
+    )
+    output=model.generate(
+        descriptions= [desc],
+        progress=True,
+        return_tokens=True
+    )
+
+    return output[0]
+
+def save_audio(samples: torch.Tensor,text):
+    sample_rate=32000
+    save_path="AudioOutput/"
+
+    assert samples.dim() ==2 or samples.dim()==3
+
+    samples = samples.detach().cpu()
+
+    if samples.dim()==2:
+        samples=samples[None,...]
+
+    for idx,audio in enumerate(samples):
+        s=text.replace(" ","_")
+        audio_path=os.path.join(save_path,f"{s}.wav")
+        torchaudio.save(audio_path,audio,sample_rate)
+
+def get_binary_file_down_html(bin_file,file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data=f.read()
+    bin_str=base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    return href
+
+def main():
+    st.title("Aryan's Music Generator 🎼")
+    text=st.text_area("Enter your description")
+    time_slider=st.slider("Select time duration (in seconds)",2,20,5)
+    if text and time_slider:
+        text_display="Your Description: "+""+text
+        st.write(text_display)
+        time_display="Time Duration of clip: "+""+str(time_slider)
+        st.write(time_display)
+        st.subheader("Generated Music")
+
+        music_tensors=generate_music_tensors(text,time_slider)
+        print("Music Tensors: ",music_tensors)
+        save_music_file=save_audio(music_tensors,text)
+        audio_filepath='AudioOutput/'+text.replace(" ","_")+'.wav'
+        audio_file=open(audio_filepath,'rb')
+        audio_bytes=audio_file.read()
+        st.audio(audio_bytes)
+        st.markdown(get_binary_file_down_html(audio_filepath,'Audio'),unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
